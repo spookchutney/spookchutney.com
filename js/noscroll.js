641d9d267a33904a3225ec49f9c4093aae9a6845
spookchutney <43921098+spookchutney@users.noreply.github.com>
2018-10-07 13:53:21 -0500

    Generate gh-pages site for d50235c

--- js/noscroll.js
+++
@@ -1,4 +0,0 @@
-// noscroll.js
-
-document.body.addEventListener('scroll', function(e){ e.preventDefault(); }, { passive: false });
-document.body.addEventListener('touchmove', function(e){ e.preventDefault(); }, { passive: false });
